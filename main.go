93f0aea62746a5d687ff3a895ecb2263638ed5ba
Anthony Holloway <avholloway@gmail.com>
2016-07-14 13:19:29 -0700

    this is dev code

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello, world123!</h1>\n")
+    fmt.Fprintf(w, "<h1>Hello, world123 - This is dev code!</h1>\n")
 }
 
 func main() {
